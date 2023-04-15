e27e6a2268464f21b648827827405ee8ba059a3f
some0necoding <codingandprogramming.code@gmail.com>
2023-04-15 17:52:15 +0200

    adding some dotfiles

--- hypr/scripts/swaylock.sh
+++ hypr/scripts/swaylock.sh
@@ -1,8 +1,17 @@
 swaylock -enf \
+	-i ~/.config/wallpapers/vectorial_light.jpg \
 	--indicator-idle-visible \
 	--inside-color 232634 \
+	--inside-clear-color e5c890 \
+	--inside-ver-color ef9f76 \
 	--key-hl-color ef9f76 \
 	--ring-color 8caaee	\
+	--ring-clear-color ef9f76 \
+	--ring-ver-color ef9f76 \
 	--separator-color ef9f76 \
-	--text-color ef9f76 \
+	--text-color 8caaee	\
+	--text-clear-color 51576d \
+	--text-ver-color 51576d \
 	--clock
+
+a6d189
