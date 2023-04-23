8940943b295726b0b15dba6598b492f7ae490d41
some0necoding <codingandprogramming.code@gmail.com>
2023-04-23 11:42:54 +0200

    fixing cursor theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 	set -gx PF_ASCII "Catppuccin"
 	set -gx XCURSOR_THEME "Breeze_Default"
+	set -gx XCURSOR_SIZE 24
 end
 fish_add_path /home/marco/.spicetify
 fish_add_path /usr/lib/jvm
