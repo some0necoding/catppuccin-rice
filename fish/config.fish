eab6fc6e6c3cd145778fd6077ba97446b00cf5ae
some0necoding <codingandprogramming.code@gmail.com>
2023-05-24 00:15:19 +0200

    setting up grim for screenshots

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
 	set -gx PF_ASCII "Catppuccin"
 	set -gx XCURSOR_THEME "Breeze_Default"
 	set -gx XCURSOR_SIZE 24
+    set -gx XDG_PICTURES_DIR ~/Pictures
 end
 
 alias ls="lsd"
