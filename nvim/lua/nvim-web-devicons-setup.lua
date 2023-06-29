380dad6d0459d6176707f47aac28ffcfa0681ec8
some0necoding <codingandprogramming.code@gmail.com>
2023-06-29 23:05:37 +0200

    just a lil refactoring

--- nvim/lua/nvim-web-devicons-setup.lua
+++ nvim/lua/nvim-web-devicons-setup.lua
@@ -1,10 +1,3 @@
 -- nvim-web-devicons-setup.lua
 
-local devicons = require('nvim-web-devicons')
-
-devicons.setup({
-	override = {
-		-- set directories' icon
-	}
-})
-
+require('nvim-web-devicons').setup()
