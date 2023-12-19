949cc0a66d339b0e0fc5bfe5707e2f3affcf06d1
some0necoding <codingandprogramming.code@gmail.com>
2023-12-19 11:19:45 +0100

    github copilot configuration

---
+++ nvim/lua/plugins-setup/copilot-setup.lua
@@ -0,0 +1,15 @@
+-- copilot-setup.lua
+
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {
+    ["*"] = false,
+    ["javascript"] = true,
+    ["typescript"] = true,
+    ["lua"] = false,
+    ["rust"] = true,
+    ["c"] = true,
+    ["c#"] = true,
+    ["c++"] = true,
+    ["go"] = true,
+    ["python"] = true,
+}
